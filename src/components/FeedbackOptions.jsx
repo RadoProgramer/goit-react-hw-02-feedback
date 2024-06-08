9f7b13c70fabf5816835067a4a8d9133978a9e5d
RadoProgramer <145141434+RadoProgramer@users.noreply.github.com>
2024-06-08 15:22:15 +0100

    Update

--- src/components/FeedbackOptions.jsx
+++ src/components/FeedbackOptions.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styles from "./Feedback.module.scss";
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
@@ -14,6 +15,11 @@ const FeedbackOptions = ({ options, onLeaveFeedback }) => {
       ))}
     </div>
   );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
 };
 
 export default FeedbackOptions;
